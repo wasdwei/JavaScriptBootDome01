7313b8075c6c77bccd40b849f3014f955581f8cc
wasdwei <2690697862@qq.com>
2023-09-16 10:46:43 +0800

    Spring MVC 功能拓展实现

---
+++ src/main/java/com/example/javascriptbootdome01/MVC/MyInterceptor.java
@@ -0,0 +1,38 @@
+package com.example.javascriptbootdome01.MVC;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Calendar;
+
+/*
+ * 自定义一个拦截器类
+ * */
+@Configuration
+public class MyInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        //用户请求/admin开头路径,判断用户是否登录
+        String uri = request.getRequestURI();
+        request.setAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+        Object loginUser = request.getSession().getAttribute("loginUser");
+        if (uri.startsWith("/admin") && null == loginUser) {
+            response.sendRedirect("/login.html");
+            return false;
+       }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        //向request域中存放当前年份用于页面动态展示
+        request.setAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+    }
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable Exception ex) throws Exception {
+    }
+}
--- src/main/java/com/example/javascriptbootdome01/Thymeleaf/LoginController.java
+++ src/main/java/com/example/javascriptbootdome01/Thymeleaf/LoginController.java
@@ -9,10 +9,10 @@ import java.util.Calendar;
 @Controller
 public class LoginController {
     //定义HTTP GET请求的URL路径
-    @GetMapping("/toLoginPage")
-    public String toLoginPage(Model model){
-        //获取并封装当前年份，跳转到登录页login.html
-        model.addAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
-        return "login";
-    }
+//    @GetMapping("/toLoginPage")
+//    public String toLoginPage(Model model){
+//        //获取并封装当前年份，跳转到登录页login.html
+//        model.addAttribute("currentYear", Calendar.getInstance().get(Calendar.YEAR));
+//        return "login";
+//    }
 }
