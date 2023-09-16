e98628abf86099c34e3fc9e93db79944bd61362d
wasdwei <2690697862@qq.com>
2023-09-17 00:19:00 +0800

    第五章SpringBoot 文件的上传

---
+++ src/main/java/com/example/javascriptbootdome01/Servlet/FileController.java
@@ -0,0 +1,49 @@
+package com.example.javascriptbootdome01.Servlet;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
+
+/*
+ * 文件管理控制类
+ * */
+@Controller
+public class FileController {
+    @GetMapping("/toUpload")
+    public String toUpload() {
+        return "upload";
+    }
+
+    //文件上传管理
+    @PostMapping("/uploadFile")
+    public String uploadFile(MultipartFile[] fileUpload, Model model) {
+        //默认文件上传成功，并返回状态信息
+        model.addAttribute("uploadStatus", "上传成功!");
+        for (MultipartFile file : fileUpload) {
+            //获取文件名以及后缀名
+            String fileName = file.getOriginalFilename();
+            //重新生成文件名（根据具体情况生成对应文件名）
+            fileName = UUID.randomUUID() + "_" + fileName;
+            //指定上传文件本地存储目录，不存在则需要提前创建
+            String dirPath = "D:/File/";
+            File filePath = new File(dirPath);
+            if (!filePath.exists()) {
+                filePath.mkdirs();
+            }
+            try {
+                file.transferTo(new File(dirPath + fileName));
+            } catch (Exception e) {
+                e.printStackTrace();
+                //上传失败,返回失败信息
+                model.addAttribute("uploadStatus", "上传失败:" + e.getMessage());
+            }
+        }
+        //携带上传状态信息回调到文件上传页面
+        return "upload";
+    }
+}
