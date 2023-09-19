5b0f72a7191e8d708c6f89df52cf45e958725792
wasdwei <2690697862@qq.com>
2023-09-19 17:39:29 +0800

    第五章SpringBoot 中文名文件的下载2.0

--- src/main/java/com/example/javascriptbootdome01/Servlet/FileController.java
+++ src/main/java/com/example/javascriptbootdome01/Servlet/FileController.java
@@ -62,7 +62,7 @@ public class FileController {
 
     //文件下载管理
     @GetMapping("/download")
-    public ResponseEntity<byte[]> fileDownload(String filename) {
+    public ResponseEntity<byte[]> fileDownload(HttpServletRequest request, String filename)throws Exception {
         //指定要下载的文件根路径
         String dirPath = "D:/File/";
         //创建该文件对象
@@ -70,6 +70,7 @@ public class FileController {
         //设置响应头
         HttpHeaders headers = new HttpHeaders();
         //通知浏览器以下载的方式打开
+        filename=getFilename(request,filename);
         headers.setContentDispositionFormData("attachment", filename);
         //定义以流的形式下载返回文件数据
         headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
@@ -80,6 +81,7 @@ public class FileController {
             return new ResponseEntity<byte[]>(e.getMessage().getBytes(), HttpStatus.EXPECTATION_FAILED);
         }
     }
+    //根据浏览器的不同进行编码后的文件名
     private String getFilename(HttpServletRequest request, String filename)
             throws Exception {
         String[] IEBrowserKeyWords = {"MSIE", "Trident", "Edge"};
