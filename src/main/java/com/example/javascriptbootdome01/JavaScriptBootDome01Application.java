96cfb745141da8aa84894f42fc97a8b95b43929c
wasdwei <2690697862@qq.com>
2023-09-20 17:09:20 +0800

    第五章SpringBoot war打包部署

--- src/main/java/com/example/javascriptbootdome01/JavaScriptBootDome01Application.java
+++ src/main/java/com/example/javascriptbootdome01/JavaScriptBootDome01Application.java
@@ -2,13 +2,19 @@ package com.example.javascriptbootdome01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication//用注解标识的类作为springboot的入口类
 @ServletComponentScan//开启基于注解方式的Servlet组件扫描支持
 @ImportResource("classpath:xmlpropertoes.xml")//加载自定义xml配置文件位置
-public class JavaScriptBootDome01Application {
+public class JavaScriptBootDome01Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+        return builder.sources(JavaScriptBootDome01Application.class);
+    }
 
     public static void main(String[] args) {
 
