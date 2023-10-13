3f58d7df7cbc52f40ac05d623f7172c5b62aa83b
wasdwei <2690697862@qq.com>
2023-10-13 21:16:06 +0800

    自定义用户认证 - JDBC身份认证

--- src/main/java/com/example/javascriptbootdome01/Security/authentication/MemorySecurityConfigurerAdapter.java
+++ src/main/java/com/example/javascriptbootdome01/Security/authentication/MemorySecurityConfigurerAdapter.java
@@ -1,21 +1,21 @@
-package com.example.javascriptbootdome01.Security.authentication;
-
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-/**
- * @Name: wql
- * @Time: 2023-10-08  11:41
- */
-@EnableWebSecurity
-public class MemorySecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("李四")
-                .password(encoder.encode("123456")).roles("common").and()
-                .withUser("张三").password(encoder.encode("123456")).roles("vip");
-    }
-}
+//package com.example.javascriptbootdome01.Security.authentication;
+//
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//
+///**
+// * @Name: wql
+// * @Time: 2023-10-08  11:41
+// */
+//@EnableWebSecurity
+//public class MemorySecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        auth.inMemoryAuthentication().passwordEncoder(encoder).withUser("李四")
+//                .password(encoder.encode("123456")).roles("common").and()
+//                .withUser("张三").password(encoder.encode("123456")).roles("vip");
+//    }
+//}
